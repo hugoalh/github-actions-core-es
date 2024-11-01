3bb6d8500dddf0943ca2f35cba26245582bdec71
hugoalh <hugoalh@gmail.com>
2024-11-01 12:19:09 +0800

    Add ability to get parameter value as JSON

--- parameter.ts
+++ parameter.ts
@@ -1,4 +1,11 @@
 import { getEnv } from "https://raw.githubusercontent.com/hugoalh/env-es/v0.2.0/env.ts";
+import {
+	isJSONPrimitive,
+	type JSONArray,
+	type JSONObject,
+	type JSONPrimitive,
+	type JSONValue,
+} from "https://raw.githubusercontent.com/hugoalh/is-json-es/v1.0.4/mod.ts";
 import { isStringSingleLine } from "https://raw.githubusercontent.com/hugoalh/is-string-singleline-es/v1.0.4/mod.ts";
 import {
 	appendFileMapCommand,
@@ -6,6 +13,12 @@ import {
 	optimizeFileCommand
 } from "./command/file.ts";
 import type { KeyValueLike } from "./common.ts";
+export type {
+	JSONArray,
+	JSONObject,
+	JSONPrimitive,
+	JSONValue
+};
 /**
  * **\[🅰️ Advanced\]** Clear the outputs which set in the current step.
  * 
@@ -53,6 +66,10 @@ export interface GitHubActionsGetParameterOptions {
 	 * - **`boolean`**: `false`
 	 * - **`number`**: `0`
 	 * - **`string`**: `""`
+	 * - **`JSONArray`**: `[]`
+	 * - **`JSONObject`**: `{}`
+	 * - **`JSONPrimitive`**: `null`
+	 * - **`JSONValue`**: `null`
 	 * 
 	 * If property {@linkcode require} is defined to `true`, define this property is pointless.
 	 * @default {true}
@@ -261,6 +278,234 @@ export function getInputBoolean(key: string, options: GitHubActionsGetParameterO
 		return true;
 	}
 	throw new SyntaxError(`Input \`${key}\` is not a valid boolean!`);
+}
+/**
+ * Get the JSON value of an input.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the input.
+ * @param {GitHubActionsGetParameterOptions & { fallback?: true; require?: false; }} [options={}] Options.
+ * @returns {JSONValue} JSON value of the input.
+ */
+export function getInputJSON(key: string, options?: GitHubActionsGetParameterOptions & { fallback?: true; require?: false; }): JSONValue;
+/**
+ * Get the JSON value of an input.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the input.
+ * @param {GitHubActionsGetParameterOptions & { require: true; }} options Options.
+ * @returns {JSONValue} JSON value of the input.
+ */
+export function getInputJSON(key: string, options: GitHubActionsGetParameterOptions & { require: true; }): JSONValue;
+/**
+ * Get the JSON value of an input.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the input.
+ * @param {GitHubActionsGetParameterOptions & { fallback: false; require?: false; }} options Options.
+ * @returns {JSONValue | undefined} JSON value of the input.
+ */
+export function getInputJSON(key: string, options: GitHubActionsGetParameterOptions & { fallback: false; require?: false; }): JSONValue | undefined;
+export function getInputJSON(key: string, options: GitHubActionsGetParameterOptions = {}): JSONValue | undefined {
+	const {
+		fallback = true,
+		require = false
+	}: GitHubActionsGetParameterOptions = options;
+	const value: string = getParameter("input", key);
+	if (value.length === 0) {
+		if (require) {
+			throw new ReferenceError(`Input \`${key}\` is not defined!`);
+		}
+		return (fallback ? null : undefined);
+	}
+	try {
+		return JSON.parse(value) as JSONValue;
+	} catch {
+		throw new SyntaxError(`Input \`${key}\` is not a valid JSON value!`);
+	}
+}
+export {
+	getInputJSON as getInputJSONValue
+};
+/**
+ * Get the JSON array of an input.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the input.
+ * @param {GitHubActionsGetParameterOptions & { fallback?: true; require?: false; }} [options={}] Options.
+ * @returns {JSONArray} JSON array of the input.
+ */
+export function getInputJSONArray(key: string, options?: GitHubActionsGetParameterOptions & { fallback?: true; require?: false; }): JSONArray;
+/**
+ * Get the JSON array of an input.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the input.
+ * @param {GitHubActionsGetParameterOptions & { require: true; }} options Options.
+ * @returns {JSONArray} JSON array of the input.
+ */
+export function getInputJSONArray(key: string, options: GitHubActionsGetParameterOptions & { require: true; }): JSONArray;
+/**
+ * Get the JSON array of an input.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the input.
+ * @param {GitHubActionsGetParameterOptions & { fallback: false; require?: false; }} options Options.
+ * @returns {JSONArray | undefined} JSON array of the input.
+ */
+export function getInputJSONArray(key: string, options: GitHubActionsGetParameterOptions & { fallback: false; require?: false; }): JSONArray | undefined;
+export function getInputJSONArray(key: string, options: GitHubActionsGetParameterOptions = {}): JSONArray | undefined {
+	const {
+		fallback = true,
+		require = false
+	}: GitHubActionsGetParameterOptions = options;
+	const value: JSONValue = getInputJSON(key, {
+		//@ts-ignore All of the overloads are fulfilled.
+		require
+	});
+	if (value === null) {
+		return (fallback ? [] : undefined);
+	}
+	if (!Array.isArray(value)) {
+		throw new SyntaxError(`Input \`${key}\` is not a valid JSON array!`);
+	}
+	return value;
+}
+/**
+ * Get the JSON object of an input.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the input.
+ * @param {GitHubActionsGetParameterOptions & { fallback?: true; require?: false; }} [options={}] Options.
+ * @returns {JSONObject} JSON object of the input.
+ */
+export function getInputJSONObject(key: string, options?: GitHubActionsGetParameterOptions & { fallback?: true; require?: false; }): JSONObject;
+/**
+ * Get the JSON object of an input.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the input.
+ * @param {GitHubActionsGetParameterOptions & { require: true; }} options Options.
+ * @returns {JSONObject} JSON object of the input.
+ */
+export function getInputJSONObject(key: string, options: GitHubActionsGetParameterOptions & { require: true; }): JSONObject;
+/**
+ * Get the JSON object of an input.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the input.
+ * @param {GitHubActionsGetParameterOptions & { fallback: false; require?: false; }} options Options.
+ * @returns {JSONObject | undefined} JSON object of the input.
+ */
+export function getInputJSONObject(key: string, options: GitHubActionsGetParameterOptions & { fallback: false; require?: false; }): JSONObject | undefined;
+export function getInputJSONObject(key: string, options: GitHubActionsGetParameterOptions = {}): JSONObject | undefined {
+	const {
+		fallback = true,
+		require = false
+	}: GitHubActionsGetParameterOptions = options;
+	const value: JSONValue = getInputJSON(key, {
+		//@ts-ignore All of the overloads are fulfilled.
+		require
+	});
+	if (value === null) {
+		return (fallback ? {} : undefined);
+	}
+	if (!(typeof value === "object" && !Array.isArray(value))) {
+		throw new SyntaxError(`Input \`${key}\` is not a valid JSON object!`);
+	}
+	return value;
+}
+/**
+ * Get the JSON primitive of an input.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the input.
+ * @param {GitHubActionsGetParameterOptions & { fallback?: true; require?: false; }} [options={}] Options.
+ * @returns {JSONPrimitive} JSON primitive of the input.
+ */
+export function getInputJSONPrimitive(key: string, options?: GitHubActionsGetParameterOptions & { fallback?: true; require?: false; }): JSONPrimitive;
+/**
+ * Get the JSON primitive of an input.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the input.
+ * @param {GitHubActionsGetParameterOptions & { require: true; }} options Options.
+ * @returns {JSONPrimitive} JSON primitive of the input.
+ */
+export function getInputJSONPrimitive(key: string, options: GitHubActionsGetParameterOptions & { require: true; }): JSONPrimitive;
+/**
+ * Get the JSON primitive of an input.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the input.
+ * @param {GitHubActionsGetParameterOptions & { fallback: false; require?: false; }} options Options.
+ * @returns {JSONPrimitive | undefined} JSON primitive of the input.
+ */
+export function getInputJSONPrimitive(key: string, options: GitHubActionsGetParameterOptions & { fallback: false; require?: false; }): JSONPrimitive | undefined;
+export function getInputJSONPrimitive(key: string, options: GitHubActionsGetParameterOptions = {}): JSONPrimitive | undefined {
+	const {
+		fallback = true,
+		require = false
+	}: GitHubActionsGetParameterOptions = options;
+	const value: JSONValue = getInputJSON(key, {
+		//@ts-ignore All of the overloads are fulfilled.
+		require
+	});
+	if (value === null) {
+		return (fallback ? null : undefined);
+	}
+	if (!isJSONPrimitive(value)) {
+		throw new SyntaxError(`Input \`${key}\` is not a valid JSON primitive!`);
+	}
+	return value;
 }
 /**
  * Get the number value of an input.
@@ -496,6 +741,234 @@ export function getStateBoolean(key: string, options: GitHubActionsGetParameterO
 		return true;
 	}
 	throw new SyntaxError(`State \`${key}\` is not a valid boolean!`);
+}
+/**
+ * Get the JSON value of a state.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the state.
+ * @param {GitHubActionsGetParameterOptions & { fallback?: true; require?: false; }} [options={}] Options.
+ * @returns {JSONValue} JSON value of the state.
+ */
+export function getStateJSON(key: string, options?: GitHubActionsGetParameterOptions & { fallback?: true; require?: false; }): JSONValue;
+/**
+ * Get the JSON value of a state.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the state.
+ * @param {GitHubActionsGetParameterOptions & { require: true; }} options Options.
+ * @returns {JSONValue} JSON value of the state.
+ */
+export function getStateJSON(key: string, options: GitHubActionsGetParameterOptions & { require: true; }): JSONValue;
+/**
+ * Get the JSON value of a state.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the state.
+ * @param {GitHubActionsGetParameterOptions & { fallback: false; require?: false; }} options Options.
+ * @returns {JSONValue | undefined} JSON value of the state.
+ */
+export function getStateJSON(key: string, options: GitHubActionsGetParameterOptions & { fallback: false; require?: false; }): JSONValue | undefined;
+export function getStateJSON(key: string, options: GitHubActionsGetParameterOptions = {}): JSONValue | undefined {
+	const {
+		fallback = true,
+		require = false
+	}: GitHubActionsGetParameterOptions = options;
+	const value: string = getParameter("state", key);
+	if (value.length === 0) {
+		if (require) {
+			throw new ReferenceError(`State \`${key}\` is not defined!`);
+		}
+		return (fallback ? null : undefined);
+	}
+	try {
+		return JSON.parse(value) as JSONValue;
+	} catch {
+		throw new SyntaxError(`State \`${key}\` is not a valid JSON value!`);
+	}
+}
+export {
+	getStateJSON as getStateJSONValue
+};
+/**
+ * Get the JSON array of a state.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the state.
+ * @param {GitHubActionsGetParameterOptions & { fallback?: true; require?: false; }} [options={}] Options.
+ * @returns {JSONArray} JSON array of the state.
+ */
+export function getStateJSONArray(key: string, options?: GitHubActionsGetParameterOptions & { fallback?: true; require?: false; }): JSONArray;
+/**
+ * Get the JSON array of a state.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the state.
+ * @param {GitHubActionsGetParameterOptions & { require: true; }} options Options.
+ * @returns {JSONArray} JSON array of the state.
+ */
+export function getStateJSONArray(key: string, options: GitHubActionsGetParameterOptions & { require: true; }): JSONArray;
+/**
+ * Get the JSON array of a state.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the state.
+ * @param {GitHubActionsGetParameterOptions & { fallback: false; require?: false; }} options Options.
+ * @returns {JSONArray | undefined} JSON array of the state.
+ */
+export function getStateJSONArray(key: string, options: GitHubActionsGetParameterOptions & { fallback: false; require?: false; }): JSONArray | undefined;
+export function getStateJSONArray(key: string, options: GitHubActionsGetParameterOptions = {}): JSONArray | undefined {
+	const {
+		fallback = true,
+		require = false
+	}: GitHubActionsGetParameterOptions = options;
+	const value: JSONValue = getStateJSON(key, {
+		//@ts-ignore All of the overloads are fulfilled.
+		require
+	});
+	if (value === null) {
+		return (fallback ? [] : undefined);
+	}
+	if (!Array.isArray(value)) {
+		throw new SyntaxError(`State \`${key}\` is not a valid JSON array!`);
+	}
+	return value;
+}
+/**
+ * Get the JSON object of a state.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the state.
+ * @param {GitHubActionsGetParameterOptions & { fallback?: true; require?: false; }} [options={}] Options.
+ * @returns {JSONObject} JSON object of the state.
+ */
+export function getStateJSONObject(key: string, options?: GitHubActionsGetParameterOptions & { fallback?: true; require?: false; }): JSONObject;
+/**
+ * Get the JSON object of a state.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the state.
+ * @param {GitHubActionsGetParameterOptions & { require: true; }} options Options.
+ * @returns {JSONObject} JSON object of the state.
+ */
+export function getStateJSONObject(key: string, options: GitHubActionsGetParameterOptions & { require: true; }): JSONObject;
+/**
+ * Get the JSON object of a state.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the state.
+ * @param {GitHubActionsGetParameterOptions & { fallback: false; require?: false; }} options Options.
+ * @returns {JSONObject | undefined} JSON object of the state.
+ */
+export function getStateJSONObject(key: string, options: GitHubActionsGetParameterOptions & { fallback: false; require?: false; }): JSONObject | undefined;
+export function getStateJSONObject(key: string, options: GitHubActionsGetParameterOptions = {}): JSONObject | undefined {
+	const {
+		fallback = true,
+		require = false
+	}: GitHubActionsGetParameterOptions = options;
+	const value: JSONValue = getStateJSON(key, {
+		//@ts-ignore All of the overloads are fulfilled.
+		require
+	});
+	if (value === null) {
+		return (fallback ? {} : undefined);
+	}
+	if (!(typeof value === "object" && !Array.isArray(value))) {
+		throw new SyntaxError(`State \`${key}\` is not a valid JSON object!`);
+	}
+	return value;
+}
+/**
+ * Get the JSON primitive of a state.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the state.
+ * @param {GitHubActionsGetParameterOptions & { fallback?: true; require?: false; }} [options={}] Options.
+ * @returns {JSONPrimitive} JSON primitive of the state.
+ */
+export function getStateJSONPrimitive(key: string, options?: GitHubActionsGetParameterOptions & { fallback?: true; require?: false; }): JSONPrimitive;
+/**
+ * Get the JSON primitive of a state.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the state.
+ * @param {GitHubActionsGetParameterOptions & { require: true; }} options Options.
+ * @returns {JSONPrimitive} JSON primitive of the state.
+ */
+export function getStateJSONPrimitive(key: string, options: GitHubActionsGetParameterOptions & { require: true; }): JSONPrimitive;
+/**
+ * Get the JSON primitive of a state.
+ * 
+ * > **🛡️ Require Runtime Permissions**
+ * > 
+ * > - Deno
+ * >   - Environment Variable (`env`)
+ * >     - *Resources*
+ * @param {string} key Key of the state.
+ * @param {GitHubActionsGetParameterOptions & { fallback: false; require?: false; }} options Options.
+ * @returns {JSONPrimitive | undefined} JSON primitive of the state.
+ */
+export function getStateJSONPrimitive(key: string, options: GitHubActionsGetParameterOptions & { fallback: false; require?: false; }): JSONPrimitive | undefined;
+export function getStateJSONPrimitive(key: string, options: GitHubActionsGetParameterOptions = {}): JSONPrimitive | undefined {
+	const {
+		fallback = true,
+		require = false
+	}: GitHubActionsGetParameterOptions = options;
+	const value: JSONValue = getStateJSON(key, {
+		//@ts-ignore All of the overloads are fulfilled.
+		require
+	});
+	if (value === null) {
+		return (fallback ? null : undefined);
+	}
+	if (!isJSONPrimitive(value)) {
+		throw new SyntaxError(`State \`${key}\` is not a valid JSON primitive!`);
+	}
+	return value;
 }
 /**
  * Get the number value of a state.
