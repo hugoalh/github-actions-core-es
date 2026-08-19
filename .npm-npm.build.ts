84d4cc535b196b0866c22b3b60e5457c4b039fbb
hugoalh <hugoalh@gmail.com>
2026-08-19 13:26:24 +0800

    Update resources

--- .npm-npm.build.ts
+++ .npm-npm.build.ts
@@ -67,5 +67,15 @@ await transform({
 		}
 	},
 	outputDirectory: "dist/npm-npm",
-	outputDirectoryPreEmpty: true
+	outputDirectoryPreEmpty: true,
+	shims: {
+		blob: false,
+		crypto: false,
+		deno: false,
+		prompts: false,
+		timers: false,
+		undici: false,
+		weakRef: false,
+		webSocket: false
+	}
 });
